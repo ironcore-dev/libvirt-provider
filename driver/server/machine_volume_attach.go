4f510afd5b6eeb16502220dbabd119f75a59f1cc
Hardik Dodiya <hardik.dodiya@sap.com>
2023-11-20 12:26:07 +0000

    Implement volume attach interface

--- driver/server/machine_volume_attach.go
+++ driver/server/machine_volume_attach.go
@@ -16,11 +16,55 @@ package server
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/onmetal/libvirt-driver/pkg/api"
 	ori "github.com/onmetal/onmetal-api/ori/apis/machine/v1alpha1"
 )
 
 func (s *Server) AttachVolume(ctx context.Context, req *ori.AttachVolumeRequest) (res *ori.AttachVolumeResponse, retErr error) {
+	log := s.loggerFrom(ctx)
+	log.V(1).Info("Attaching volume to machine")
+
+	if req == nil || req.MachineId == "" || req.Volume == nil {
+		return nil, fmt.Errorf("invalid request")
+	}
+
+	apiMachine, err := s.machineStore.Get(ctx, req.MachineId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get machine: %w", err)
+	}
+
+	// Convert the Ori Volume to Api VolumeSpec
+	var connectionSpec *api.VolumeConnection
+	if connection := req.Volume.Connection; connection != nil {
+		connectionSpec = &api.VolumeConnection{
+			Driver:     connection.Driver,
+			Handle:     connection.Handle,
+			Attributes: connection.Attributes,
+			SecretData: connection.SecretData,
+		}
+	}
+
+	var emptyDiskSpec *api.EmptyDiskSpec
+	if emptyDisk := req.Volume.EmptyDisk; emptyDisk != nil {
+		emptyDiskSpec = &api.EmptyDiskSpec{
+			Size: emptyDisk.SizeBytes,
+		}
+	}
+
+	volumeSpec := &api.VolumeSpec{
+		Name:       req.Volume.Name,
+		Device:     req.Volume.Device,
+		EmptyDisk:  emptyDiskSpec,
+		Connection: connectionSpec,
+	}
+
+	apiMachine.Spec.Volumes = append(apiMachine.Spec.Volumes, volumeSpec)
+
+	if _, err := s.machineStore.Update(ctx, apiMachine); err != nil {
+		return nil, fmt.Errorf("failed to update machine with new volume: %w", err)
+	}
 
 	return &ori.AttachVolumeResponse{}, nil
 }
