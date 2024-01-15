baaaeac98d6c00e0ef437109056a7c861c84bc37
Kajol Asabe <kajol.asabe@t-systems.com>
2024-01-15 21:52:45 +0530

    Add integration tests for AttachVolume and DetachVolume

---
+++ provider/server/machine_volume_attach_test.go
@@ -0,0 +1,135 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package server_test
+
+import (
+	"os"
+
+	"github.com/digitalocean/go-libvirt"
+	iri "github.com/ironcore-dev/ironcore/iri/apis/machine/v1alpha1"
+	irimeta "github.com/ironcore-dev/ironcore/iri/apis/meta/v1alpha1"
+	libvirtutils "github.com/ironcore-dev/libvirt-provider/pkg/libvirt/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"libvirt.org/go/libvirtxml"
+)
+
+var _ = Describe("AttachVolume", func() {
+	var (
+		cephMonitors = os.Getenv("CEPH_MONITORS")
+		cephImage    = os.Getenv("CEPH_IMAGE")
+		cephUsername = os.Getenv("CEPH_USERNAME")
+		cephUserkey  = os.Getenv("CEPH_USERKEY")
+	)
+	It("should correctly attach volume to machine", func(ctx SpecContext) {
+		By("creating a machine")
+		createResp, err := machineClient.CreateMachine(ctx, &iri.CreateMachineRequest{
+			Machine: &iri.Machine{
+				Metadata: &irimeta.ObjectMetadata{
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+				},
+				Spec: &iri.MachineSpec{
+					Power: iri.Power_POWER_ON,
+					Class: machineClassx3xlarge,
+				},
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(createResp).NotTo(BeNil())
+
+		DeferCleanup(func(ctx SpecContext) {
+			Eventually(func() bool {
+				_, err := machineClient.DeleteMachine(ctx, &iri.DeleteMachineRequest{MachineId: createResp.Machine.Metadata.Id})
+				Expect(err).ShouldNot(HaveOccurred())
+				_, err = libvirtConn.DomainLookupByUUID(libvirtutils.UUIDStringToBytes(createResp.Machine.Metadata.Id))
+				return libvirt.IsNotFound(err)
+			}).Should(BeTrue())
+		})
+
+		By("ensuring domain and domain XML is created for machine")
+		var domain libvirt.Domain
+		Eventually(func() error {
+			domain, err = libvirtConn.DomainLookupByUUID(libvirtutils.UUIDStringToBytes(createResp.Machine.Metadata.Id))
+			return err
+		}).Should(Succeed())
+		domainXMLData, err := libvirtConn.DomainGetXMLDesc(domain, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(domainXMLData).NotTo(BeEmpty())
+
+		By("ensuring domain for machine is in running state")
+		Eventually(func() libvirt.DomainState {
+			domainState, _, err := libvirtConn.DomainGetState(domain, 0)
+			Expect(err).NotTo(HaveOccurred())
+			return libvirt.DomainState(domainState)
+		}).Should(Equal(libvirt.DomainRunning))
+
+		By("attaching empty disk to a machine")
+		attachEmptyDiskResp, err := machineClient.AttachVolume(ctx, &iri.AttachVolumeRequest{
+			MachineId: createResp.Machine.Metadata.Id,
+			Volume: &iri.Volume{
+				Name: "disk-1",
+				EmptyDisk: &iri.EmptyDisk{
+					SizeBytes: 5368709120,
+				},
+				Device: "oda",
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(attachEmptyDiskResp).NotTo(BeNil())
+
+		By("ensuring machine is in running state")
+		Eventually(func() iri.MachineState {
+			resp, err := machineClient.ListMachines(ctx, &iri.ListMachinesRequest{
+				Filter: &iri.MachineFilter{
+					Id: createResp.Machine.Metadata.Id,
+				},
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.Machines).NotTo(BeEmpty())
+			return resp.Machines[0].Status.State
+		}).Should(Equal(iri.MachineState_MACHINE_RUNNING))
+
+		By("attaching volume with connection details to a machine")
+		attachVolumeConnectionResp, err := machineClient.AttachVolume(ctx, &iri.AttachVolumeRequest{
+			MachineId: createResp.Machine.Metadata.Id,
+			Volume: &iri.Volume{
+				Name:   "volume-1",
+				Device: "odb",
+				Connection: &iri.VolumeConnection{
+					Driver: "ceph",
+					Handle: "dummy",
+					Attributes: map[string]string{
+						"image":    cephImage,
+						"monitors": cephMonitors,
+					},
+					SecretData: map[string][]byte{
+						"userID":  []byte(cephUsername),
+						"userKey": []byte(cephUserkey),
+					},
+				},
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(attachVolumeConnectionResp).NotTo(BeNil())
+
+		By("ensuring both the disks are attached to a machine domain")
+		var disks []libvirtxml.DomainDisk
+		Eventually(func() int {
+			domainXMLData, err := libvirtConn.DomainGetXMLDesc(domain, 0)
+			Expect(err).NotTo(HaveOccurred())
+			domainXML := &libvirtxml.Domain{}
+			err = domainXML.Unmarshal(domainXMLData)
+			Expect(err).NotTo(HaveOccurred())
+			disks = domainXML.Devices.Disks
+			return len(disks)
+		}).Should(Equal(2))
+		Expect(disks[0].Serial).To(HavePrefix("oda"))
+		Expect(disks[1].Serial).To(HavePrefix("odb"))
+
+		// TODO - ensuring volume spec and status is updated in iri machine to be done
+		// after convertMachineToIRIMachine() supports Volumes and NetworkInterfaces
+	})
+})
