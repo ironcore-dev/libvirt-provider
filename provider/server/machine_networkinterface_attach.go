4a0ae38ca96b589572849aad199b14ba0072116d
Lukas Frank <lukas.frank@sap.com>
2024-01-15 11:16:33 +0100

    Added NIC Attach/Detach
    Signed-off-by: Lukas Frank <lukas.frank@sap.com>

--- provider/server/machine_networkinterface_attach.go
+++ provider/server/machine_networkinterface_attach.go
@@ -5,11 +5,34 @@ package server
 
 import (
 	"context"
+	"fmt"
 
 	iri "github.com/ironcore-dev/ironcore/iri/apis/machine/v1alpha1"
 )
 
 func (s *Server) AttachNetworkInterface(ctx context.Context, req *iri.AttachNetworkInterfaceRequest) (res *iri.AttachNetworkInterfaceResponse, retErr error) {
+	log := s.loggerFrom(ctx)
+	log.V(1).Info("Attaching NIC to machine")
+
+	if req == nil {
+		return nil, fmt.Errorf("AttachNetworkInterfaceRequest is nil")
+	}
+
+	apiMachine, err := s.machineStore.Get(ctx, req.MachineId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get machine: %w", err)
+	}
+
+	nicSpec, err := s.getNICFromIRINIC(req.NetworkInterface)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get nic from iri nic: %w", err)
+	}
+
+	apiMachine.Spec.NetworkInterfaces = append(apiMachine.Spec.NetworkInterfaces, nicSpec)
+
+	if _, err := s.machineStore.Update(ctx, apiMachine); err != nil {
+		return nil, fmt.Errorf("failed to update machine: %w", err)
+	}
 
 	return &iri.AttachNetworkInterfaceResponse{}, nil
 }
