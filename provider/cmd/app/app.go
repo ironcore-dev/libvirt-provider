e55e4a213e3bb1807a9c2c77f538cb08991d16f7
Lukas Koszegy <l.koszegy@gmail.com>
2024-03-06 09:05:29 +0000

    start metrics server, when address is set

--- provider/cmd/app/app.go
+++ provider/cmd/app/app.go
@@ -101,6 +101,7 @@ type HTTPServerOptions struct {
 type ServersOptions struct {
 	Metrics HTTPServerOptions
 }
+
 type LibvirtOptions struct {
 	Socket  string
 	Address string
@@ -125,7 +126,7 @@ func (o *Options) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&o.BaseURL, "base-url", "", "The base url to construct urls for streaming from. If empty it will be "+
 		"constructed from the streaming-address")
 
-	fs.StringVar(&o.Servers.Metrics.Addr, "servers-metrics-address", ":8080", "Address to listen on exposing of metrics.")
+	fs.StringVar(&o.Servers.Metrics.Addr, "servers-metrics-address", "", "Address to listen on exposing of metrics. If address isn't set, server is disabled.")
 	fs.DurationVar(&o.Servers.Metrics.ReadTimeout, "servers-metrics-readtimeout", 200*time.Millisecond, "Read timeout for metrics server.")
 	fs.DurationVar(&o.Servers.Metrics.WriteTimeout, "servers-metrics-writetimeout", 200*time.Millisecond, "Write timeout for metrics server.")
 	fs.DurationVar(&o.Servers.Metrics.IdleTimeout, "server-metrics-idletimeout", 1*time.Second, "Idle timeout for connections to metrics server.")
@@ -477,6 +478,11 @@ func runStreamingServer(ctx context.Context, setupLog, log logr.Logger, srv *ser
 }
 
 func runMetricsServer(ctx context.Context, setupLog logr.Logger, opts HTTPServerOptions) error {
+	if opts.Addr == "" {
+		setupLog.Info("Metrics server address isn't configured. Metrics server is disabled.")
+		return nil
+	}
+
 	setupLog.Info("Starting metrics server on " + opts.Addr)
 
 	mux := http.NewServeMux()
