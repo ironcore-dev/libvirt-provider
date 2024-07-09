ad7c56dc2f02521a37043be52ed9eee44ba2203c
Parik Cmil <patrik.cmil@t-systems.com>
2024-07-09 08:56:44 +0000

    update gracefulShutdownTimeout

--- tests/integration/server_suite_test.go
+++ tests/integration/server_suite_test.go
@@ -38,7 +38,7 @@ const (
 	emptyDiskSize   = 1024 * 1024 * 1024
 
 	//Test graceful shutdown will be tested separately
-	gracefulShutdownTimeout        = 0
+	gracefulShutdownTimeout        = 60 * time.Second
 	resyncGarbageCollectorInterval = 5 * time.Second
 	resyncVolumeSizeInterval       = 1 * time.Minute
 	baseURL                        = "http://localhost:20251"
