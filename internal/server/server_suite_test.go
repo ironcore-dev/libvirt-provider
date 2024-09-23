f3f10729b9d5c614107248651a455b5d092ac961
Lukas Frank <lukas.frank@sap.com>
2024-09-23 15:09:56 +0200

    fix

--- internal/server/server_suite_test.go
+++ internal/server/server_suite_test.go
@@ -53,6 +53,7 @@ var (
 	machineClient      iriv1alpha1.MachineRuntimeClient
 	libvirtConn        *libvirt.Libvirt
 	machineClassesFile *os.File
+	apinetKubeconfigFile *os.File
 	tempDir            string
 	cephMonitors       = os.Getenv("CEPH_MONITORS")
 	cephImage          = os.Getenv("CEPH_IMAGE")
@@ -99,6 +100,9 @@ var _ = BeforeSuite(func() {
 	DeferCleanup(machineClassesFile.Close)
 	DeferCleanup(os.Remove, machineClassesFile.Name())
 
+	apinetKubeconfigFile, err = os.CreateTemp(GinkgoT().TempDir(), "apinet-kubeconfig")
+	Expect(err).NotTo(HaveOccurred())
+
 	pluginOpts := networkinterfaceplugin.NewDefaultOptions()
 	pluginOpts.PluginName = "isolated"
 
@@ -109,6 +113,7 @@ var _ = BeforeSuite(func() {
 		Address:                     filepath.Join(tempDir, "test.sock"),
 		BaseURL:                     baseURL,
 		PathSupportedMachineClasses: machineClassesFile.Name(),
+		ApinetKubeconfig:            apinetKubeconfigFile.Name(),
 		RootDir:                     filepath.Join(tempDir, "libvirt-provider"),
 		StreamingAddress:            streamingAddress,
 		Servers: app.ServersOptions{
