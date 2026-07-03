c2760019cd75fa9e7adcc08761273a54b5a841e7
Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>
2026-07-03 17:54:44 +0200

    Filter in store List calls instead of post-fetch loops
    Signed-off-by: Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>

--- internal/utils/claims.go
+++ internal/utils/claims.go
@@ -9,19 +9,18 @@ import (
 	"github.com/ironcore-dev/libvirt-provider/api"
 	"github.com/ironcore-dev/provider-utils/claimutils/pci"
 	hostutils "github.com/ironcore-dev/provider-utils/storeutils/host"
+	"github.com/ironcore-dev/provider-utils/storeutils/store"
 )
 
 func GetClaimedPCIAddressesFromMachineStore(ctx context.Context, machineStore *hostutils.Store[*api.Machine]) ([]pci.Address, error) {
-	machines, err := machineStore.List(ctx)
+	machines, err := machineStore.List(ctx, store.MatchingFields{api.MachineSpecHasGpuField: "true"})
 	if err != nil {
 		return nil, err
 	}
 
 	var pciAddrs []pci.Address
 	for _, machine := range machines {
-		if machine.Spec.Gpu != nil {
-			pciAddrs = append(pciAddrs, machine.Spec.Gpu...)
-		}
+		pciAddrs = append(pciAddrs, machine.Spec.Gpu...)
 	}
 	return pciAddrs, nil
 }
